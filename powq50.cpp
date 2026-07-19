7d03a08d6340d7f25e23beb7a4ea7164514ba309
Anmol gupta <anmol542005@gmail.com>
2026-07-19 23:17:30 +0530

    added 0 and 1 base condition

--- powq50.cpp
+++ powq50.cpp
@@ -4,6 +4,8 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0) return 1.0;
+        if(x==0) return 0;
+        if(x==1) return 1;
         long binform =n;
         if(n<0){
             x=1/x;
