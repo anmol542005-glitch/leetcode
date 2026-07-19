3f5041e68ede7787c1002e3a3352ca5a906417bd
Anmol gupta <anmol542005@gmail.com>
2026-07-19 23:19:14 +0530

    added power 1 condition

--- powq50.cpp
+++ powq50.cpp
@@ -6,6 +6,7 @@ public:
         if(n==0) return 1.0;
         if(x==0) return 0;
         if(x==1) return 1;
+        if(n==1) return x;
         long binform =n;
         if(n<0){
             x=1/x;
