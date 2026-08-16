ea9d09588f914366f62f587a88e129b42c89e6b0
Anmol gupta <anmol542005@gmail.com>
2026-08-16 23:21:17 +0530

    single element done

--- singleelement.cpp
+++ singleelement.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int singleelement(vector<int>arr){
-    int str=0,end=arr.size()-1;
+int single_element(vector<int>arr){
+    int n=arr.size();
+    int str=0,end=n-1;
+    if(n==1) return arr[0];
     while(str<=end){
         int mid=str+(end-str)/2;
+        if(mid==0 && arr[0] !=arr[1]) return mid;
+        if(mid==n-1 && arr[n-1]!=arr[n-2]) return mid;
         if(arr[mid-1]!=arr[mid] && arr[mid+1]!=arr[mid]){
             return mid;
         }
